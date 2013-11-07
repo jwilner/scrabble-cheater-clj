9896ee7a26d907e8c295a4bd4c47f632696651c3
Joe Wilner <jhwilner1@gmail.com>
2013-11-07 16:08:26 -0500

    Reorganized the score map declaration.

--- src/scrabble_cheater/core.clj
+++ src/scrabble_cheater/core.clj
@@ -7,27 +7,10 @@
 (def valid-chars (into (vector wildcard)
                        (map #(str (char %)) (range 90 64 -1))))
 (def max-num-chars 9)
-
-(def char-map {(apply concat (for [[n vec-letters]  
-                                     [[2 [\D \G]]
-                                      [3 [\B \C \M \P]]
-                                      [4 [\F \H \V \W \Y]]
-                                      [5 [\K]]
-                                      [8 [\J \X]]
-                                      [10 [\Q \Z]]]]
-                                    (map #(vector (str n) %) 
-                                       vec-letters)))})
-
-
-(def special-letters 
-            {\C 3, \B 3, \D 2, \G 2, \F 4, \H 4, \K 5, \J 8, \M 3,
-             \Q 10, \P 3, \W 4, \V 4, \Y 4, \X 8, \Z 10})
-(def scores (into {} (concat  
-                        (for [c valid-chars
-                              :when (and (not (contains? special-letters c))
-                                         (not= c wildcard))] 
-                              [c 1])
-                        special-letters)))
+(def scores {"A" 1, "B" 3, "C" 3, "D" 2, "E" 1, "F" 4, "G" 2, "H" 4,
+             "I" 1, "J" 10, "K" 5, "L" 1, "M" 3, "N" 1, "O" 1, "P" 3,
+             "Q" 10, "R" 1, "S" 1, "T" 1, "U" 1, "V" 4, "W" 4, "X" 8,
+             "Y" 4, "Z" 10})
 
 (defn -main
   "I don't do a whole lot ... yet."
