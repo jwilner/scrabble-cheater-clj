0e4b33adc70ba4a0321ba0483f38ce66a7d16ac5
Joe Wilner <jhwilner1@gmail.com>
2013-11-09 18:13:00 -0500

    Added a lot more functionality

--- test/scrabble_cheater/core_test.clj
+++ test/scrabble_cheater/core_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [scrabble-cheater.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def match-triples [[true? "WANDER" 0 "WANDER"]
+                    [true? "A" 2 "BE"]
+                    [true? "WANDER" 3 "WANDERING" ]
+                    [true? "ASODIJ" 0 "SODA" ]
+                    [true? "A" "CATERING" 8]
+                    [true? "CATSASDF" "CATS" 0]
+                    [false? "A" "GERMAN" 0]
+                    [false? "C" "LITERALLY" 2])
+
+(deftest match-testing
+  (testing
+    "The main match function. Too bad doing it this way 
+    you can't see the literal values."
+    (dorun (map (partial apply #(is (%1 (match? (seq %2)
+                                                (seq %3)
+                                                %4))))
+                 match-triples))))
