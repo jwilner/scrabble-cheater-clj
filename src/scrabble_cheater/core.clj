9e57a4aca7f33af1deef066892b783336cada272
Joe Wilner <jhwilner1@gmail.com>
2013-11-10 21:27:23 -0500

    Rethinking match? function to make larger process more efficient.

--- src/scrabble_cheater/core.clj
+++ src/scrabble_cheater/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as string]))
 
+;; global values
 (def wildcard \@)
 (def path-to-dict "dictionaries/sowpods.txt")
 (def max-num-chars 9)
@@ -9,53 +10,93 @@
              \I 1, \J 10, \K 5, \L 1, \M 3, \N 1, \O 1, \P 3,
              \Q 10, \R 1, \S 1, \T 1, \U 1, \V 4, \W 4, \X 8,
              \Y 4, \Z 10})
-
 (def valid-characters (set (cons wildcard 
                                  (keys scores))))
 
-(defn remove-first [x coll]
+(defn print-func [i word score]
+  (println (format "%d. '%s' = %d"  
+                   i
+                   word
+                   score)))
+
+;; main functions
+(defn remove-first
   "Remove the first instance of a value from a collection. If the value
   isn't present, do nothing. Copped from
   https://groups.google.com/d/msg/clojure/nkANE4GxFmU/7YgW6UMA560J"  
+  [x coll]
   (let [[pre post] (split-with #(not= x %)
                                coll)]
     (concat pre (rest post))))
 
-(defn match? [rack wcs word]
-  "Tail recursive function taking a rack sequence, a word sequence, and the 
-  number of wildcards to allow. Returns truthy value." 
+(defn match? 
+  "Tail recursive function taking a rack sequence, the number of 
+  wildcards to allow, and a word sequence. Returns truthy value." 
+  [rack wcs word]
   (let [enough-wildcards (>= wcs 0) 
         character (first word)] 
     (if (or (nil? character) 
             (not enough-wildcards))
-        enough-wildcards
+        enough-wildcards ;; enough-wildcards always holds the return value
         (recur (remove-first character rack) ;; Only changes if present 
-               (rest word)
                (if (some #{character} rack)
                    wcs
-                   (dec wcs))))))
+                   (dec wcs))
+               (rest word)))))
 
-(defn score-word [word]
-  (reduce + (map scores word)))
+(defn letter-intersection
+  "Takes the rack, the word, and the number of wildcards. Returns
+  nil if unable to use wildcards, else returns list of present letters
+  ready for scoring."
+  [rack num-wcs word]
+  (loop [letter (first word)
+         remaining rack 
+         remaining-wcs num-wcs
+         intersected []]
+    (if (nil? letter)
+      intersected
+      (if (some #{letter} rack)
+        (recur (remove-first character 
+                            rack)
+              num-wcs
+              (rest word))
+        (if (pos? num-wcs)
+          (recur rack
+                (dec num-wcs)
+                (rest word))
+          nil)))))
+
+
+(defn score-letters [letters]
+  (reduce + (map scores letters)))
 
 (defn open-dictionary [dictionary-path]
   (with-open [rdr (clojure.java.io/reader dictionary-path)]
     (line-seq rdr)))
 
-(defn get-match-list [list-of-words rack]
-  (let [num-wcs (count (filter #(= wildcard %) 
-                               rack))]
+(defn get-match-list 
+  "Takes the list of words and the rack, filters using match? function,
+  pairs words in tuples with their scores, and then sorts on those scores 
+  in descending order."
+  [list-of-words rack]
+  (let [[num-wcs tame-rack] ((juxt #(count (filter %1 %2))
+                                   remove) 
+                             #{wildcard} rack))
+        matches (filter (partial match? 
+                                 rack 
+                                 num-wcs)
+                        list-of-words)]
       (sort-by second 
                >
-               (map #(list % (score-word %))
-                    (filter (partial match? rack num-wcs)
-                            list-of-words)))))
+               (map #(vector % 
+                             (score-letters %))
+                    matches))))
 
 
 #_(defn -main
   "I don't do a whole lot ... yet."
   [& args]
-    (doseq [[i [word score]] (map-indexed #(list (inc %1) %2)
+    (doseq [[i [word score]] (map-indexed #(vector (inc %1) %2)
                                           (get-match-list dictionary-path
                                                           rack))]
-          (println (format "%d. '%s' = %d points" i word score))))
+          (println (format print-string i word score))))
--- test/scrabble_cheater/core_test.clj
+++ test/scrabble_cheater/core_test.clj
@@ -6,16 +6,42 @@
                     [true? "A" 2 "BE"]
                     [true? "WANDER" 3 "WANDERING" ]
                     [true? "ASODIJ" 0 "SODA" ]
-                    [true? "A" "CATERING" 8]
-                    [true? "CATSASDF" "CATS" 0]
-                    [false? "A" "GERMAN" 0]
-                    [false? "C" "LITERALLY" 2])
+                    [true? "A" 8 "CATERING"]
+                    [true? "CATSASDF" 0 "CATS"]
+                    [false? "A" 0 "GERMAN"]
+                    [false? "C" 2 "LITERALLY"]])
 
-(deftest match-testing
+(def letters-scores [["CAB" 7]
+                     ["CHEATER" 12]])
+
+(def letter-intersection [["" 0 "A" nil]
+                          ["A" 0 "B" nil]
+                          ["A" 0 "" []]
+                          ["A" 1 "B" []]
+                          ["A" 0 "A" [\A]]
+                          ["AA" 0 "A" [\A]]
+                          ["A" 1 "AA" [\A]]
+                          ["ABC" 1 "AB" [\A \B]]
+                          ["A" 8 "LITERALLY" [\A]]
+                          ["A" 7 "LITERALLY" nil]
+                          ["ACHIR" 1 "CHAIR" [\C \H \A \I \R]]
+                          ["ACHIS" 1 "CHAIRS" [\C \H \A \I \S]]
+                          ["ACHIS" 0 "CHAIRS" nil])
+
+(deftest match-test
   (testing
     "The main match function. Too bad doing it this way 
     you can't see the literal values."
     (dorun (map (partial apply #(is (%1 (match? (seq %2)
-                                                (seq %3)
-                                                %4))))
+                                                %3 
+                                                (seq %4)))))
                  match-triples))))
+
+(deftest score-letters-test
+  (testing
+    "Simple function that scores word using HO funcs"
+    (dorun (map (partial apply #(is (= (score-letters %1)
+                                       %2)))
+                letters-scores))))
+
+
